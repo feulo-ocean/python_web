a63aafa9a850d0d0d0fafbe7f2ff90f52b319982
feulo-ocean <guilherme.feulo@oceanbrasil.com>
2023-02-15 22:38:08 +0000

    meio da aula 2

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+SECRET_KEY = "pudim"
+app.config.from_object(__name__)
 
 # POSTS MOCK
 posts = [
@@ -17,7 +19,20 @@ posts = [
         "texto": "Novo Post"
     }
 ]
+# USER MOCKS
+USERNAME = "admin"
+PASSWORD = "admin"
 
 @app.route('/')
 def exibir_entradas():
     return render_template("exibir_entradas.html", entradas=posts)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = ""
+    if request.method == "POST":
+        if request.form['username'] == USERNAME and request.form['password'] == PASSWORD:
+            session['logado'] = True
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou senha inválidos"
+    return render_template("login.html", erro=erro)
